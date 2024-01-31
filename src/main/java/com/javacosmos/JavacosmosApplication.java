c450297d1236360a9df36612ba994c1093b967ea
Gustavo Toyota <gustavottoyota@gmail.com>
2024-01-31 11:04:42 -0400

    feat: add @SpringBootApplication explanation

--- src/main/java/com/javacosmos/JavacosmosApplication.java
+++ src/main/java/com/javacosmos/JavacosmosApplication.java
@@ -3,7 +3,16 @@ package com.javacosmos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * The @SpringBootApplication annotation is equivalent to using @Configuration, @EnableAutoConfiguration, and @ComponentScan with their default attributes.
+ */
+
 @SpringBootApplication
+// Try commenting out the @SpringBootApplication annotation and uncommenting the
+// following three annotations to see what happens:
+// @Configuration
+// @EnableAutoConfiguration
+// @ComponentScan
 public class JavacosmosApplication {
 
 	public static void main(String[] args) {
