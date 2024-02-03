8c424baaf11c701a3cfcfd0af6098a1d1a61d5a3
Gustavo Toyota <gustavottoyota@gmail.com>
2024-02-03 17:21:07 -0400

    feat: improve AOP logs

--- src/main/java/com/javacosmos/springaop/AspectExample.java
+++ src/main/java/com/javacosmos/springaop/AspectExample.java
@@ -21,42 +21,46 @@ public class AspectExample {
   //
   // @Before("pointcut()")
   // public void beforeAdvice(JoinPoint joinPoint) {
-  // System.out.println("AOP log: " + joinPoint.getSignature().getName() + "
+  // System.out.println("AOP log: method " + joinPoint.getSignature().getName() +
+  // "
   // (Before advice)");
   // }
 
   @Before(value = "execution(* com.javacosmos..*(..))")
   public void beforeAdvice(JoinPoint joinPoint) {
-    System.out.println("AOP log: " + joinPoint.getSignature().getName() + " (Before advice)");
+    System.out.println("AOP log: method " + joinPoint.getSignature().getName() + " (Before advice)");
   }
 
   @After(value = "execution(* com.javacosmos..*(..))")
   public void afterAdvice(JoinPoint joinPoint) {
-    System.out.println("AOP log: " + joinPoint.getSignature().getName() + " (After advice)");
+    System.out.println("AOP log: method " + joinPoint.getSignature().getName() + " (After advice)");
   }
 
   @AfterReturning(value = "execution(* com.javacosmos..*(..))", returning = "result")
   public void afterReturningAdvice(JoinPoint joinPoint, Object result) {
     System.out
-        .println("AOP log: " + joinPoint.getSignature().getName() + " (After returning advice). Result: " + result);
+        .println(
+            "AOP log: method " + joinPoint.getSignature().getName() + " (After returning advice). Result: " + result);
   }
 
   @AfterThrowing(value = "execution(* com.javacosmos..*(..))", throwing = "exception")
   public void afterThrowingAdvice(JoinPoint joinPoint, Throwable exception) {
     System.out
         .println(
-            "AOP log: " + joinPoint.getSignature().getName() + " (After throwing advice). Exception: " + exception);
+            "AOP log: method " + joinPoint.getSignature().getName() + " (After throwing advice). Exception: "
+                + exception);
   }
 
   @Around(value = "execution(* com.javacosmos..*(..))")
   public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
     System.out
         .println(
-            "AOP log: " + proceedingJoinPoint.getSignature().getName() + " (Around advice: Before invoking method)");
+            "AOP log: method " + proceedingJoinPoint.getSignature().getName()
+                + " (Around advice: Before invoking method)");
 
     Object result = proceedingJoinPoint.proceed();
 
-    System.out.println("AOP log: " + proceedingJoinPoint.getSignature().getName()
+    System.out.println("AOP log: method " + proceedingJoinPoint.getSignature().getName()
         + " (Around advice: After invoking method). Result: " + result);
 
     return result;
