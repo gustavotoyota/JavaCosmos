ad5aebda282539b41c95a101ac40f811f07c8bc7
Gustavo Toyota <gustavottoyota@gmail.com>
2024-02-04 16:53:04 -0400

    feat: improve home page

--- src/main/java/com/javacosmos/springweb/controllermappings/HelloWorldController.java
+++ src/main/java/com/javacosmos/springweb/controllermappings/HelloWorldController.java
@@ -1,43 +1,40 @@
 package com.javacosmos.springweb.controllermappings;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloWorldController {
   @GetMapping("/")
   public String get() {
-    return "<div>JavaCosmos links:</div>" +
-        "<br/>" +
-        "<div><a href=\"/jsp\">JSP example</a></div>" +
-        "<div><a href=\"/thymeleaf\">Thymeleaf example</a></div>" +
-        "<br/>" +
-        "<div><a href=\"/files\">File upload</a></div>" +
-        "<br/>" +
-        "<div><a href=\"/form-auth/test\">Form login</a></div>" +
-        "<div><a href=\"/basic-auth/test\">Basic HTTP auth</a></div>";
+    return "home-page/home-page.html";
   }
 
   @PostMapping("/")
+  @ResponseBody
   public String post() {
     return "Hello World! POST method";
   }
 
   @PutMapping("/")
+  @ResponseBody
   public String put() {
     return "Hello World! PUT method";
   }
 
   @DeleteMapping("/")
+  @ResponseBody
   public String delete() {
     return "Hello World! DELETE method";
   }
 
   @PatchMapping("/")
+  @ResponseBody
   public String patch() {
     return "Hello World! PATCH method";
   }
